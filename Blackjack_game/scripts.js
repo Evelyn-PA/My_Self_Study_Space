212c4bde7d2665a46b13bf5f5d15d4f35cd4b331
Evelyn6165 <phuonganhnguyen6165@gmail.com>
2025-02-07 22:05:42 +0200

    Update to make the project work at the beginning

--- Blackjack_game/scripts.js
+++ Blackjack_game/scripts.js
@@ -1,15 +1,41 @@
 let firstCard = 2
 let secondCard = 10
 let sum = firstCard + secondCard + 1
+let hasBlackJack = false
+let isAlive = true
 
-//console.log(sum)
+// Store the el-messages & sum-el & cards-el in variable called messageEl
+let messageEl = document.getElementById("el-messages")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card?")
+// Declaire variables and assign its values to an emty string
+let message = ""
+
+//create startGame function
+function startGame() {
+    //Display the sum on the screen
+    sumEl.textContent = "Sum: " + sum
+
+    //Display the cards on the screen
+    cardsEl.textContent = "Cards: " + firstCard + "-" + secondCard
+    if (sum < 21) {
+        message = "Do you want to draw a new card?"
+        console.log(message)
+    }
+    else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+        console.log(message)
+    }
+    else {
+        message = "You're out of the game!"
+        isAlive = false
+        console.log(message)
+    }
+
+    //Display the message on the screen
+    messageEl.textContent = message
 }
-else if (sum === 21) {
-    console.log("You've got Blackjack!")
-}
-else {
-    console.log("You're out of the game!")
-}
+
+
