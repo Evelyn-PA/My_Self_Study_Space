15a3623f500637efa2de50fe5d8983bd453a4d7c
Evelyn6165 <phuonganhnguyen6165@gmail.com>
2025-02-19 23:33:43 +0200

    Practice some demo value

--- Chrome_Extension/scripts.js
+++ Chrome_Extension/scripts.js
@@ -1,4 +1,16 @@
-function save() {
-    // let inputEl = document.querySelector("#save-el");
-    console.log("button clicked");
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector("#input-btn");
+const ulEl = document.querySelector("#ul-el");
+
+let saveEl = document.querySelector("#save-el");
+saveEl.addEventListener("click", function () {
+    console.log("Button clicked with addEventListener");
+    myLeads.push(inputEl.value);
+    console.log(myLeads);
+})
+
+//Log out the items in myLeads array
+for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i]);
 }
