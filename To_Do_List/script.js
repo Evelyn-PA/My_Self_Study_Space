6a20a35c570d4c7685c7e9d92b03e99aefc5897e
Evelyn6165 <phuonganhnguyen6165@gmail.com>
2025-03-03 12:30:15 +0200

    Add the checked function to the app

---
+++ To_Do_List/script.js
@@ -0,0 +1,9 @@
+let listEl = document.querySelectorAll("#list li") // select all the list items
+
+// loop through the list items and add an event listener to each one
+// if the list item already has the class "checked", remove it
+listEl.forEach(function (item) {
+    item.addEventListener("click", function () { // when the list item is clicked, add the class "checked" to the list item
+        this.classList.toggle("checked")
+    })
+})
