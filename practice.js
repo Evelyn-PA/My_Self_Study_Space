7b3c1d747ef6b587a79f17b1b7c06790b501e02f
Evelyn6165 <phuonganhnguyen6165@gmail.com>
2025-03-15 21:07:53 +0200

    Do some excercise

--- practice.js
+++ practice.js
@@ -146,3 +146,27 @@ console.log(countingDown());
 console.log(countingDown());
 console.log(countingDown());
 
+//------------------------//
+// Challenge: 
+// 1. Create three variables - bar, cafe, restaurant - and set their values to a bar, a cafe and a restaurant in your city
+// 2. Create an object - favouritePlaces - and populate it with your variables from task 1
+// 3. If you're not already using the object property shorthand, refactor your code to do so
+// 4. Create a method on the object - greeting() -  that logs out a message to the console
+// 5. Invoke the greeting() method and also log out your favourite restaurant to the console
+
+// Write your code below:
+let bar = "Kahu"
+let cafe = "Expresso"
+let restaurant = "Sushi buffet"
+
+const favouritePlaces = {
+    bar,
+    cafe,
+    restaurant,
+    //Create method on the object
+    greeting: () => { console.log("Hello") }
+}
+
+console.log(favouritePlaces)
+favouritePlaces.greeting()
+console.log(favouritePlaces.restaurant)
