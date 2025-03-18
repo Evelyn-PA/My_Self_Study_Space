949c5a626ee6a1b52ba57c7336bc42bac8d4b4b9
Evelyn6165 <phuonganhnguyen6165@gmail.com>
2025-03-18 14:52:36 +0200

    Initial feature

--- To_Do_List/script.js
+++ To_Do_List/script.js
@@ -1,4 +1,4 @@
-
+let checkList = document.querySelector("#list")
 // Create popup box in the center of the screen to add new tasks
 let modal = document.querySelector(".modal-container")
 let btn = document.querySelector("#add-btn")
@@ -6,8 +6,11 @@ let closeBtn = document.querySelector("#close-btn")
 const saveBtn = document.querySelector("#done-btn")
 let input = document.querySelector("#input")
 const list = document.querySelector("#list")
+const highBtn = document.querySelector("#high-btn")
+const mediumBtn = document.querySelector("#medium-btn")
+const lowBtn = document.querySelector("#low-btn")
 let myOutPut = []
-
+let currentColor = ""
 //When the add btn clicked, show the modal
 btn.addEventListener("click", () =>
     modal.style.display = "block")
@@ -16,31 +19,47 @@ btn.addEventListener("click", () =>
 closeBtn.addEventListener("click", () =>
     modal.style.display = "none")
 
+
+highBtn.addEventListener("click", function () {
+    currentColor = "#FE4A49"; // Red color for high priority
+});
+mediumBtn.addEventListener("click", function () {
+    currentColor = "#FED766"; // Yellow color for medium priority
+});
+lowBtn.addEventListener("click", function () {
+    currentColor = "#009FB7"; // Blue color for low priority
+});
+
 //When the user click done, the input will appear to the screen
 // And the modal will close
 saveBtn.addEventListener("click", function () {
-    myOutPut.push(input.value)
-    input.value = " "
+    myOutPut.push({ text: input.value, color: currentColor })
+    input.value = ""
     render(myOutPut)
+    //Close the modal
+    modal.style.display = "none"
+    //reset the color
+    currentColor = ""
 })
+
+
+
+//Feature to tick the task when it's done
+checkList.addEventListener("click", function (event) {
+    if (event.target.tagName === "LI") {
+        event.target.classList.toggle("checked");
+    }
+});
 
 //Function render the input
 function render(input) {
-    let outputItems = " "
+    let outputItems = ""
     for (let i = 0; i < input.length; i++) {
         outputItems += `
-        <li>${input[i]}</li>
+        <li style = "color: ${input[i].color}">${input[i].text}</li>
         `
     }
     list.innerHTML = outputItems
-    let listEl = document.querySelectorAll("#list li") // select all the list items
-    // loop through the list items and add an event listener to each one
-    // if the list item already has the class "checked", remove it
-    listEl.forEach(function (item) {
-        item.addEventListener("click", function () { // when the list item is clicked, add the class "checked" to the list item
-            this.classList.toggle("checked")
-        })
-    })
 
 }
 
