22880c3ca2c6c329f17bb477aeae03b24311a5e2
Evelyn6165 <phuonganhnguyen6165@gmail.com>
2025-03-23 23:08:22 +0200

    Add Delete Icon to the task - Update

--- To_Do_List/script.js
+++ To_Do_List/script.js
@@ -11,6 +11,7 @@ const mediumBtn = document.querySelector("#medium-btn")
 const lowBtn = document.querySelector("#low-btn")
 let myOutPut = []
 let currentColor = ""
+const deleteBtn = document.querySelector(".delete-btn")
 //When the add btn clicked, show the modal
 btn.addEventListener("click", () =>
     modal.style.display = "block")
@@ -52,14 +53,28 @@ saveBtn.addEventListener("click", function () {
 
 
 //Feature to tick the task when it's done
+// checkList.addEventListener("click", function (event) {
+//     if (event.target.tagName === "LI") {
+//         event.target.classList.toggle("checked");
+//         const index = Array.from(event.target.parentNode.children).indexOf(event.target);//Check location of the items
+//         myOutPut[index].checked = !myOutPut[index].checked; // Slipt the checked status of the items
+
+//     }
+//     else if (event.target.tagName === "SPAN") {
+//         event.target.parentElement.remove()
+//     }
+//     localStorage.setItem("myOutPut", JSON.stringify(myOutPut))
+// }
+// );
+
 checkList.addEventListener("click", function (event) {
     if (event.target.tagName === "LI") {
-        event.target.classList.toggle("checked");
-        const index = Array.from(event.target.parentNode.children).indexOf(event.target);//Check location of the items
-        myOutPut[index].checked = !myOutPut[index].checked; // Slipt the checked status of the items
-        localStorage.setItem("myOutPut", JSON.stringify(myOutPut))
+        event.target.classList.toggle("checked")
     }
-});
+    else if (event.target.tagName === "I") {
+        event.target.closest("li").remove()
+    }
+})
 
 //Function render the input
 //Add a class, check if the item was checked -> apply the checked class
@@ -69,7 +84,7 @@ function render(input) {
         outputItems += ` 
         <li style="color: ${input[i].color}" class="${input[i].checked ? 'checked' : ''}">
         ${input[i].text}
-        <span><i class="fa-solid fa-trash delete-btn" style="color: #00424d;"></i><span>
+        <span><i class="fa-solid fa-trash delete-btn" style="color: #00424d;"></i></span>
         </li>
         `
     }
