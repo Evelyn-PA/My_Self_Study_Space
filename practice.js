27dfbefb678ad3736e952afc0876d80ea52458dd
Evelyn6165 <phuonganhnguyen6165@gmail.com>
2025-03-30 23:32:28 +0300

    Practice class in JS

--- practice.js
+++ practice.js
@@ -460,16 +460,36 @@
 // book1.removeThemes("Fiction")
 // console.log(book1)
 
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = function(nums, target) {
-    for(var i = 0; i++; i<nums.length){
-        var number = nums[i]++
-        if(number === target){
-            return number 
-        }
+// 1. A school's film club wants to store details of the films it has studied so far this year. Create a new class to do this. We want to store the following data about each film: id, title, director, releaseYear and genres.
+
+// 2. Create two methods on the class: one for adding multiple genres to the films - addGenre(genre) - and one to get the title of the film - getFilmTitle().
+
+// 3. Instantiate a new instance of the class using data from your your favourite film. Add at least 1 genre to your film using addGenre(), and get the title using getFilmTitle()
+
+// Beginning:
+class Film {
+    //Your code here.
+    constructor(id, title, director, releaseYear, genres = []) {
+        this.id = id
+        this.title = title
+        this.director = director
+        this.releaseYear = releaseYear
+        this.genres = Array.isArray(genres) ? genres : [];
     }
-}; 
+
+    // Create method
+    addGenre(genre) {
+        this.genres = [... this.genres, genre];
+    }
+
+    //Create getFilmTitle
+    getFilmTitle() {
+        console.log(this.title)
+    }
+}
+
+// Rest of your code here. 
+const film1 = new Film(1, "Hello", "JK", 2003, ["fiction"])
+film1.getFilmTitle()
+film1.addGenre("novel")
+console.log(film1)
