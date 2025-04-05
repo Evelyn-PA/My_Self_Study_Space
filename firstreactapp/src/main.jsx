4e694211fde8e4e3412150c7e195b5a0c26074a3
Evelyn6165 <phuonganhnguyen6165@gmail.com>
2025-04-05 23:35:44 +0300

    First project with React - Create list

--- firstreactapp/src/main.jsx
+++ firstreactapp/src/main.jsx
@@ -1,3 +1,8 @@
 import { createRoot } from 'react-dom/client'
-const root = createRoot(document.getElementById('root'))
-root.render(<h1>Hello, React!</h1>)
+createRoot(document.getElementById('root')).render(
+  <ul>
+    <li>The most common framework to lear</li>
+    <li>Help me to secure a job in front-end field</li>
+    <li>Has lots of materials and document to learn this framework</li>
+  </ul>
+)
