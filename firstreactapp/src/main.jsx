aa3ca99c50337ee1f72fdf353d8c973c4d09e17c
Evelyn6165 <phuonganhnguyen6165@gmail.com>
2025-04-07 22:40:53 +0300

    Create element and render in JSX

--- firstreactapp/src/main.jsx
+++ firstreactapp/src/main.jsx
@@ -1,8 +1,7 @@
+// import { createElement } from "react" //Similar with document.createElement
 import { createRoot } from 'react-dom/client'
-createRoot(document.getElementById('root')).render(
-  <ul>
-    <li>The most common framework to lear</li>
-    <li>Help me to secure a job in front-end field</li>
-    <li>Has lots of materials and document to learn this framework</li>
-  </ul>
-)
+const root = createRoot(document.getElementById("root"))
+const reactElement = <h1>Hello everyone from React!!!!</h1>
+console.log(reactElement)
+
+root.render(reactElement)
