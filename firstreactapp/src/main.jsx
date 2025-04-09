3918311c0684155b78b8f0aefa4a9d93c9d0769d
Evelyn6165 <phuonganhnguyen6165@gmail.com>
2025-04-09 16:19:29 +0300

    Create a new component

--- firstreactapp/src/main.jsx
+++ firstreactapp/src/main.jsx
@@ -11,18 +11,25 @@ import { createRoot } from "react-dom/client"
 import "./index.css";
 const root = createRoot(document.getElementById("root"))
 root.render(
-  <main>
-    <div className="header">
-      <img src="\images\tips.png" alt="mylogo" />
-    </div>
-    <div className="content">
-      <h1>Fun Facts about me</h1>
-      <ul>
-        <li>I don't want to do the handwork.</li>
-        <li>I want to become a software developer.</li>
-        <li>I want to travel around the world.</li>
-        <li>I want to live a peaceful life.</li>
-      </ul>
-    </div>
-  </main>
-) 
+  temporaryName()
+)
+
+//create component
+function temporaryName() {
+  return (
+    <main>
+      <div className="header">
+        <img src="\images\tips.png" alt="mylogo" />
+      </div>
+      <div className="content">
+        <h1>Fun Facts about me</h1>
+        <ul>
+          <li>I don't want to do the handwork.</li>
+          <li>I want to become a software developer.</li>
+          <li>I want to travel around the world.</li>
+          <li>I want to live a peaceful life.</li>
+        </ul>
+      </div>
+    </main>
+  )
+}
