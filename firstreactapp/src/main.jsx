dbf0ff4a60fc35b59af892e0c9c4b0bf343f81f8
Evelyn6165 <phuonganhnguyen6165@gmail.com>
2025-04-09 15:32:10 +0300

    innitial setup for header

--- firstreactapp/src/main.jsx
+++ firstreactapp/src/main.jsx
@@ -11,7 +11,18 @@ import { createRoot } from "react-dom/client"
 import "./index.css";
 const root = createRoot(document.getElementById("root"))
 root.render(
-  <main><img src="\images\tips.png" />
-    <h1>This is another element</h1>
+  <main>
+    <div className="header">
+      <img src="\images\tips.png" alt="mylogo" />
+    </div>
+    <div className="content">
+      <h1>Fun Facts about me</h1>
+      <ul>
+        <li>I don't want to do the handwork.</li>
+        <li>I want to become a software developer.</li>
+        <li>I want to travel around the world.</li>
+        <li>I want to live a peaceful life.</li>
+      </ul>
+    </div>
   </main>
 ) 
