4ccff89dda57bc627c1b116a6a53b2946424cefd
Evelyn6165 <phuonganhnguyen6165@gmail.com>
2025-04-09 18:24:32 +0300

    Do the simple static page with React

--- firstreactapp/src/main.jsx
+++ firstreactapp/src/main.jsx
@@ -7,15 +7,45 @@
  *    an h1 element that says anything you want.
  */
 
+/**
+ * Challenge (part 1):
+ * Create a custom "Page" component
+ * 
+ * It should return an ordered list with the reasons why you're
+ * excited to be learning React :)
+ * 
+ * Render the Page component.
+ */
+
+/**
+Challenge: 
+
+Part 2: 
+- Add a `<header>` element with an `<img />` element with the image of the 
+  React logo inside (src="react-logo.png") and make sure to set the 
+  width to something more manageable so it doesn't take up the whole screen.
+  Also, as always, you should include some alt text on the image.
+- Add an `<h1>` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list, then wrap
+  the `<h1>` and `<ol>` inside a `<main>` element to keep our semantic
+  structure flowing well.
+- Add a `<footer>` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+ */
+
 import { createRoot } from "react-dom/client"
 import "./index.css";
 const root = createRoot(document.getElementById("root"))
 root.render(
-  temporaryName()
+  <>
+    <TemporaryName />
+    <Footer />
+  </>
+
 )
 
 //create component
-function temporaryName() {
+function TemporaryName() {
   return (
     <main>
       <div className="header">
@@ -29,7 +59,17 @@ function temporaryName() {
           <li>I want to travel around the world.</li>
           <li>I want to live a peaceful life.</li>
         </ul>
+
       </div>
     </main>
+  )
+}
+
+//create a custom "page" component
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 Evelyn Nguyen development. All rights reserved.</p>
+    </footer>
   )
 }
