25369289edf3e16a8ae43273beeab90c8e8e8dff
Evelyn6165 <phuonganhnguyen6165@gmail.com>
2025-04-15 16:03:07 +0300

    Create static pages project

---
+++ Static_Pages/App.jsx
@@ -0,0 +1,15 @@
+import Main from "./Components/Main"
+import Navbar from "./Components/Navbar"
+
+//Let this can be export to index.jsx
+export default function App() {
+    //import component
+    return (
+        <>
+            <Navbar />
+            <Main />
+        </>
+    )
+
+}
+
---
+++ Static_Pages/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function app() {
+    return (
+        <header>
+            <nav>
+                <h1>Nav bar goes here</h1>
+            </nav>
+        </header>
+    )
+}
