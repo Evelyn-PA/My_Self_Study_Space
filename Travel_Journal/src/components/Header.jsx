2bfab6dc90d1def193b6fca6fdc353804165bf4a
Evelyn6165 <phuonganhnguyen6165@gmail.com>
2025-04-21 12:20:05 +0300

    Make header for the site

---
+++ Travel_Journal/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <>
+            <h2>My Travel Journal <img src="/Images/travel.png" alt="Travel Icon" width="20" /></h2>
+        </>
+    )
+}
