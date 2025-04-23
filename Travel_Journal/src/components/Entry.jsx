11adcd3943abe6c0634d79bb2e8c9e33b699210f
Evelyn6165 <phuonganhnguyen6165@gmail.com>
2025-04-24 01:25:19 +0300

    allign center to the content

--- Travel_Journal/src/components/Entry.jsx
+++ Travel_Journal/src/components/Entry.jsx
@@ -2,7 +2,7 @@ export default function Entry() {
     return (
         <>
             <div className="photos">
-                <img src="\Images\Finland - Helsinki.jpg" alt="Finland" width="200" />
+                <img src="\Images\Finland - Helsinki.jpg" alt="Finland" />
             </div>
             <div className="content">
                 <div className="head">
@@ -11,9 +11,10 @@ export default function Entry() {
                     <a href="https://www.google.com/maps/place/Helsinki/@60.1100963,24.689046,10z/data=!3m1!4b1!4m6!3m5!1s0x46920bc796210691:0xcd4ebd843be2f763!8m2!3d60.
                     1698557!4d24.9383791!16zL20vMDNraG4?entry=ttu&g_ep=EgoyMDI1MDQxNi4xIKXMDSoASAFQAw%3D%3D" target="_blank">View on Google Maps</a>
                 </div>
+
                 <h1>Helsinki</h1>
-                <h4>22 Apr, 2025 - 22 May, 2025</h4>
-                <p>A crisp, stylish travel journal capturing days spent in Helsinki—where Nordic design meets old-world charm.
+                <p style={{ fontWeight: "bold" }}> 22 Apr, 2025 - 22 May, 2025</p>
+                <p>A crisp, stylish travel journal capturing days spent in Helsinki, where Nordic design meets old-world charm.
                     From sauna sessions by the sea to strolls through snowy streets and modern cafés, this journal holds notes, sketches,
                     and moments from Finland’s cool capital.</p>
             </div>
