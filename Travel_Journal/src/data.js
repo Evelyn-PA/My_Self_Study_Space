c1c1848d9a7efb438bd5145e5eba0b6d1ec59ec2
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-05-02 23:49:03 +0300

    Add data to the app

---
+++ Travel_Journal/src/data.js
@@ -0,0 +1,57 @@
+export default [
+    {
+        id: 1,
+        img: "\Images\Finland - Helsinki.jpg",
+        title: "Helsinki - Finland",
+        name: "Helsinki",
+        link: "https://shorturl.at/B1Baj",
+        content: `A crisp, stylish travel journal capturing days spent in Helsinki, where Nordic design meets old-world charm.
+                    From sauna sessions by the sea to strolls through snowy streets and modern cafés, this journal holds notes, sketches,
+        and moments from Finland’s cool capital.`
+    },
+    {
+        id: 2,
+        img: "/Images/Tampere.jpg",
+        title: "Tampere - Finland",
+        name: "Tampere",
+        link: "https://shorturl.at/DlAJL",
+        content: `Industrial roots meet natural beauty. I wandered between red-brick factories turned into art hubs and serene lakeside views. 
+                    A laid-back city with a creative edge.`
+    },
+    {
+        id: 3,
+        img: "/Images/Finland - Hameenlinna.jpg",
+        title: "Hameenlinna - Finland",
+        name: "Hameenlinna",
+        link: "https://shorturl.at/SAISk",
+        content: `A quiet stop rich in history. The medieval castle stands strong, overlooking peaceful parks and lakes. 
+                    Small-town Finland with a strong sense of identity.`
+    },
+    {
+        id: 4,
+        img: "/Images/Kuopio.jpg",
+        title: "Kuopio - Finland",
+        name: "Kuopio",
+        link: "https://shorturl.at/1Qv8x",
+        content: `Sauna capital and lake views for days. I hiked up Puijo Tower, cruised the waters, and soaked 
+                    in that calm Finnish pace. Warm people, even warmer steam.`
+    },
+    {
+        id: 5,
+        img: "/Images/Stockholm.jpg",
+        title: "Stockholm - Sweden",
+        name: "Stockholm",
+        link: "https://shorturl.at/nFsht",
+        content: `Stylish, historic, and full of energy. From the cobblestones of Gamla Stan to sleek modern districts, 
+                  every turn had something worth seeing(and photographing).`
+    },
+    {
+        id: 6,
+        img: "/Images/Talin.jpg",
+        title: "Tallinn - Estonia",
+        name: "Tallinn",
+        link: "https://tinyurl.com/3ufex9sk",
+        content: `A fairytale vibe with a modern twist. The Old Town is all towers and stone walls, 
+                  but just beyond are hip cafes and Baltic buzz.Old meets new, perfectly.`
+    }
+]
