5bc4de1148e433b995ecc82da5a36248176e1e4e
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-05-09 23:44:19 +0300

    Change style for the form and btn

--- chef_claude/src/components/Main.jsx
+++ chef_claude/src/components/Main.jsx
@@ -1,13 +1,19 @@
 export default function Main() {
+
+    function handleClick(event) {
+        event.preventDefault();
+        console.log("Button clicked");
+    }
+
     return (
         <>
-            <form className="form">
+            <form className="form" onSubmit={handleClick}>
                 <input
                     type="text"
                     placeholder="Enter your ingredients"
                     aria-label="Add ingredient"
                 />
-                <button>+ Add</button>
+                <button onClick={handleClick}>+ Add</button>
             </form>
         </>
     )
