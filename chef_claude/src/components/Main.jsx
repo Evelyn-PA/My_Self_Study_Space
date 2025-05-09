b08a849584693786e716b46561cb4ae5fbb361cc
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-05-10 00:33:16 +0300

    Add ingredients to the app

--- chef_claude/src/components/Main.jsx
+++ chef_claude/src/components/Main.jsx
@@ -1,20 +1,36 @@
 export default function Main() {
+    const ingreadients = ["eggs", "milk", "flour", "sugar", "butter"];
+    const ingredientsList = ingreadients.map((ingredient) => {
+        return (
+            <li key={ingredient}>
+                {ingredient}
+            </li>
+        )
+    })
 
-    function handleClick(event) {
+    function handleSubmit(event) {
         event.preventDefault();
-        console.log("Button clicked");
+        const formData = new FormData(event.currentTarget); //Web API FormData to get data from the form
+        const newIngredient = formData.get("ingredient");
+        ingreadients.add(newIngredient); //Add the new ingredient to the list
+        console.log(ingreadients);
     }
 
     return (
         <>
-            <form className="form" onSubmit={handleClick}>
+            <form className="form" onSubmit={handleSubmit}>
                 <input
                     type="text"
                     placeholder="Enter your ingredients"
+                    name="ingredient"
                     aria-label="Add ingredient"
                 />
-                <button onClick={handleClick}>+ Add</button>
+                <button type="submit">+ Add</button>
             </form>
+
+            <ul>
+                {ingredientsList}
+            </ul>
         </>
     )
 }
