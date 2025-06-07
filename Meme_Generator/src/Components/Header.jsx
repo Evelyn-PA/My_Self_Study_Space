2ffcc54a2df258ef494ddf322a8a6af12a9f7c8e
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-06-07 16:22:47 +0300

    Create component files, create header for the website

---
+++ Meme_Generator/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="public/Icon/meme.png" alt="Meme Icon" id="header-icon" />
+            <div className="web-name">
+                <h1>Meme Generator</h1>
+                <h3>Serious tools for unserious minds.</h3>
+            </div>
+
+        </header>
+    )
+}
