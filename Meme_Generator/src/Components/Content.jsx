4b93a9d25fb44c13aae07066e99d885f59da88ca
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-06-09 19:46:43 +0300

    adjust the input box and style

--- Meme_Generator/src/Components/Content.jsx
+++ Meme_Generator/src/Components/Content.jsx
@@ -1,27 +1,34 @@
 export default function Content() {
     return (
         <main>
-            <div>
-                <form>
+            <div className="form">
+
+                <form className="form-input">
                     <label>
                         Top Text
-                        <input type="text"
-                            placeholder="Me trying to fix my life"
-                            name="topText" />
                     </label>
-                    <lable>
+                    <input type="text"
+                        placeholder="Me trying to fix my life"
+                        name="topText" />
+
+
+                    <label>
                         Bottom Text
-                        <input
-                            type="text"
-                            placeholder="With my broken brain"
-                            name="bottomText"
-                        />
-                    </lable>
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="With my broken brain"
+                        name="bottomText"
+                    />
+
                 </form>
-
                 <button>Get me a new meme image</button>
+            </div>
 
-
+            <div className="meme">
+                <img src="https://i.imgflip.com/9wp95f.jpg" title="made at imgflip.com" />
+                <span className="top">Me trying to fix my life</span>
+                <span className="bottom">With my broken brain</span>
             </div>
         </main>
     )
