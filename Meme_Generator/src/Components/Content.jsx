906d482f3aa0b3d06679f006602fb56f0dce8c61
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-06-10 14:17:50 +0300

    update the input text to same time appear to the meme

--- Meme_Generator/src/Components/Content.jsx
+++ Meme_Generator/src/Components/Content.jsx
@@ -2,46 +2,59 @@ import { useState } from "react"
 
 export default function Content() {
 
-        const [meme, setMeme] = useState({
-            topText: "Me trying to fix my life",
-            bottomText: "With my broken brain",
-            imgURL:"https://i.imgflip.com/9wp95f.jpg " 
-        })
-        return (
-            <main>
-                <div className="form">
+    const [meme, setMeme] = useState({
+        topText: "Me trying to fix my life",
+        bottomText: "With my broken heart",
+        imgURL: "https://i.imgflip.com/9wp95f.jpg "
+    })
 
-                    <form className="form-input">
-                        <div className="form-box">
-                            <label>
-                                Top Text
-                            </label>
-                            <input type="text"
-                                placeholder="Me trying to fix my life"
-                                name="topText" />
-                        </div>
-
-                        <div className="form-box">
-                            <label>
-                                Bottom Text
-                            </label>
-                            <input
-                                type="text"
-                                placeholder="With my broken heart"
-                                name="bottomText"
-                            />
-                        </div>
-
-
-                    </form>
-                    <button>Get me a new meme image</button>
-                </div>
-
-                <div className="meme">
-                    <img src= {meme.imgURL}/>
-                    <span className="top"> {meme.topText}</span>
-                    <span className="bottom">{meme.bottomText}</span>
-                </div>
-            </main>
-        )
+    function handleChange(event) {
+        const { value, name } = event.currentTarget
+        setMeme(prevText => ({
+            ...prevText,
+            [name]: value
+        }))
     }
+    return (
+        <main>
+            <div className="form">
+
+                <form className="form-input"> 
+                    <div className="form-box">
+                        <label>
+                            Top Text
+                        </label>
+                        <input type="text"
+                            placeholder="Me trying to fix my life"
+                            name="topText"
+                            onChange={handleChange}
+                            value ={meme.topText}
+                        />
+                    </div>
+
+                    <div className="form-box">
+                        <label>
+                            Bottom Text
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="With my broken heart"
+                            name="bottomText"
+                            onChange={handleChange}
+                            value={meme.bottomText}
+                        />
+                    </div>
+
+
+                </form>
+                <button>Get me a new meme image</button>
+            </div>
+
+            <div className="meme">
+                <img src={meme.imgURL} />
+                <span className="top"> {meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
+            </div>
+        </main>
+    )
+}
