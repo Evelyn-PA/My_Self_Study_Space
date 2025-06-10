7f4a3d9b38b296b8770207ecbcb798141ef76e38
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-06-10 13:55:02 +0300

    add text to state

--- Meme_Generator/src/Components/Content.jsx
+++ Meme_Generator/src/Components/Content.jsx
@@ -1,35 +1,47 @@
+import { useState } from "react"
+
 export default function Content() {
-    return (
-        <main>
-            <div className="form">
 
-                <form className="form-input">
-                    <label>
-                        Top Text
-                    </label>
-                    <input type="text"
-                        placeholder="Me trying to fix my life"
-                        name="topText" />
+        const [meme, setMeme] = useState({
+            topText: "Me trying to fix my life",
+            bottomText: "With my broken brain",
+            imgURL:"https://i.imgflip.com/9wp95f.jpg " 
+        })
+        return (
+            <main>
+                <div className="form">
+
+                    <form className="form-input">
+                        <div className="form-box">
+                            <label>
+                                Top Text
+                            </label>
+                            <input type="text"
+                                placeholder="Me trying to fix my life"
+                                name="topText" />
+                        </div>
+
+                        <div className="form-box">
+                            <label>
+                                Bottom Text
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="With my broken heart"
+                                name="bottomText"
+                            />
+                        </div>
 
 
-                    <label>
-                        Bottom Text
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="With my broken brain"
-                        name="bottomText"
-                    />
+                    </form>
+                    <button>Get me a new meme image</button>
+                </div>
 
-                </form>
-                <button>Get me a new meme image</button>
-            </div>
-
-            <div className="meme">
-                <img src="https://i.imgflip.com/9wp95f.jpg" title="made at imgflip.com" />
-                <span className="top">Me trying to fix my life</span>
-                <span className="bottom">With my broken brain</span>
-            </div>
-        </main>
-    )
-}
+                <div className="meme">
+                    <img src= {meme.imgURL}/>
+                    <span className="top"> {meme.topText}</span>
+                    <span className="bottom">{meme.bottomText}</span>
+                </div>
+            </main>
+        )
+    }
