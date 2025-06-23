36e33daa5e944d049130695dd9a095b2f54c5e5c
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-06-23 22:25:04 +0300

    Create Die component and render 6 of it

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,6 +1,16 @@
-export default function App(){
+import data from "./data"
+import Die from "./Components/Die"
+export default function App() {
+  const dieValue = data.map((dt) => {
+    return <Die
+      key={dt.id}
+      {...dt}
+    />
+  })
   return (
-    <main></main>
+    <main>
+      {dieValue}
+    </main>
   );
 }
 
