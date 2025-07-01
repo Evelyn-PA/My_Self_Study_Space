323098d7ef88a9bf6cbf4b72111bbf055f6b6ed1
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-07-01 14:41:27 +0300

    Add the words to the app

--- Assembly-Endgame/src/App.jsx
+++ Assembly-Endgame/src/App.jsx
@@ -1,16 +1,28 @@
 import Header from "./Components/Header"
 import Tag from "./Components/Tags"
+import { nanoid } from "nanoid"
 import "./App.css"
+
 import { languages } from "./language"
+import { useState } from "react"
 
 export default function App() {
+
+  const [words, setWords] = useState("React")
   const LanguageData = languages.map((dt) => {
-    return <Tag 
-      name ={dt.name}
-      backgroundColor ={dt.backgroundColor}
-      color = {dt.color}
-      />
+    return <Tag
+      key={nanoid()}
+      name={dt.name}
+      backgroundColor={dt.backgroundColor}
+      color={dt.color}
+    />
   })
+
+  const letterElement = words.split("").map((letter, index )=> (
+    <span key ={index}>{letter.toUpperCase()}</span>
+  ))
+
+
   return (
 
     <div>
@@ -21,6 +33,11 @@ export default function App() {
       <div className="tags-container">
         {LanguageData}
       </div>
+      <section className="words">
+        {letterElement}
+      </section>
+
+
     </div>
 
   )
